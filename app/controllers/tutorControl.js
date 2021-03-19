2445b0906c9ff920e2b0b4d74e17b0a7b87b1e3c
Salvatore <48434490+Salvo1108@users.noreply.github.com>
2021-03-19 18:41:47 +0100

    Copy Project

---
+++ app/controllers/tutorControl.js
@@ -0,0 +1,222 @@
+var hash = require('./hash.js')
+var ExternalTutorModel = require('../models/externaltutor.js')
+var OrganizationModel = require('../models/hostorganization.js')
+
+/**
+ * This method inserts an host organization
+ * @param {Object} req - The HTTP req
+ * @param {Object} res - The HTTP res
+ * @returns {Boolean}  - This method returns true if the insert of host organization was successfull, else false
+ */
+exports.addHostOrg = function (req, res) {
+  return new Promise(function (resolve, reject) {
+    var erasmusCode = req.body.inputErasmusCode
+    var faculty = req.body.inputFacolta
+    var address = req.body.inputAddress
+    var orgSize = req.body.inputSize
+    var country = req.body.inputCountry
+    var contact = req.body.inputContacts
+    var name = req.body.inputNameT
+
+    var isRight = true
+
+    if ((erasmusCode == null) || (erasmusCode.length <= 3) || (!/^[A-Za-z0-9]+$/.test(erasmusCode))) {
+      res.cookie('errErasmusCode', '1')
+      isRight = false
+    }
+
+    if ((name == null) || (name.length <= 1) || (!/^[A-Za-z]+$/.test(name))) {
+      res.cookie('errTutorName', '1')
+      isRight = false
+    }
+
+    if ((faculty == null) || (faculty.length <= 1) || (!/^[A-Za-z]+$/.test(faculty))) {
+      res.cookie('errFacolta', '1')
+      isRight = false
+    }
+
+    if ((address == null) || (address.length <= 6) || (!/^[A-Za-z0-9,\s]+$/.test(address))) {
+      res.cookie('errHAddress', '1')
+      isRight = false
+    }
+
+    if ((contact == null) || (contact.length <= 7) || (!/(^[0-9]{9,10}$|[a-z0-9!#$%&'*+=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9]){1,}?)/.test(contact))) {
+      res.cookie('errContatti', '1')
+      isRight = false
+    }
+
+    if ((country == null) || (country.length <= 1) || (!/^[A-Za-z0-9\s]+$/.test(country))) {
+      res.cookie('errCountryName', '1')
+      isRight = false
+    }
+
+    if ((orgSize == null) || (orgSize == 0) || (!/^[0-9\s]+$/.test(orgSize))) {
+      res.cookie('errSize', '1')
+      isRight = false
+    }
+
+    if (!isRight) {
+      resolve(false)
+      return
+    }
+
+    // Create host organization object
+    var organizzazioneEsterna = new OrganizationModel()
+    organizzazioneEsterna.setContacts(contact)
+    organizzazioneEsterna.setName(name)
+    organizzazioneEsterna.setCountry(country)
+    organizzazioneEsterna.setOrgSize(orgSize)
+    organizzazioneEsterna.setAddress(address)
+    organizzazioneEsterna.setFaculty(faculty)
+    organizzazioneEsterna.setErasmusCode(erasmusCode)
+
+    var checkE = OrganizationModel.findByEcode(erasmusCode)
+
+    checkE.then(function (result) {
+      if (!result) {
+        res.cookie('errAlreadyRegH', '1')
+        resolve(false)
+        return
+      }
+      if (result) {
+        // Save student in database
+        OrganizationModel.addHostOrg(organizzazioneEsterna)
+        resolve(true)
+      }
+    })
+  })
+}
+
+/**
+ * This method inserts an external tutor
+ * @param {Object} req - The HTTP req
+ * @param {Object} res - The HTTP res
+ * @returns {Boolean}  - This method returns true if the insert of external tutor was successfull, else false
+ */
+exports.addExtTutor = function (req, res) {
+  return new Promise(function (resolve, reject) {
+    var name = req.body.inputNameEx
+    var surname = req.body.inputSurnameEx
+    var email = req.body.inputEmailEx
+    var password = req.body.inputPassword
+    var repassword = req.body.inputRePassword
+    var organization = req.body.inputOrganizationEx
+
+    var isRight = true
+    if (password != repassword) {
+      isRight = false
+    }
+
+    isRight = true
+    if ((name == null) || (name.length <= 1) || (!/^[A-Za-z]+$/.test(name))) {
+      res.cookie('errExTutorName', '1')
+      isRight = false
+    }
+
+    if ((surname == null) || (surname.length <= 1) || (!/^[A-Za-z]+$/.test(surname))) {
+      res.cookie('errExTutorSurname', '1')
+      isRight = false
+    }
+
+    if ((email == null) || (email.length <= 6) || (!/[a-z0-9!#$%&'*+=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9]){1,}?/.test(email))) {
+      res.cookie('errExTutorEmail', '1')
+      isRight = false
+    }
+
+    if ((organization == null) || (organization.length <= 1) || (!/^[A-Za-z0-9\s]+$/.test(organization))) {
+      res.cookie('errExOrganizationName', '1')
+      isRight = false
+    }
+
+    if ((password == null) || (password.length <= 7) || (!/^[A-Za-z0-9]+$/.test(password))) {
+      res.cookie('errPassword', '1')
+      isRight = false
+    }
+
+    if (repassword != password) {
+      res.cookie('errPasswordConfirm', '1')
+      isRight = false
+    }
+
+    if (!isRight) {
+      resolve(false)
+      return
+    }
+
+    // hashing e salt of password
+    var passwordHashed = hash.hashPassword(password)
+
+    // Create external tutor object
+    var tutorEsterno = new ExternalTutorModel()
+    tutorEsterno.setSurname(surname)
+    tutorEsterno.setName(name)
+    tutorEsterno.setOrganization(organization)
+    tutorEsterno.setEmail(email)
+    tutorEsterno.setPassword(passwordHashed)
+
+    var checkE = ExternalTutorModel.findByEmailA(email)
+    checkE.then(function (result) {
+      if (!result) {
+        resolve(false)
+        return
+      }
+      if (result) {
+        // Save tutor in database
+        ExternalTutorModel.addExtTutor(tutorEsterno)
+        resolve(true)
+      }
+    })
+  })
+}
+
+/**
+ * This method deletes an host organization
+ * @param {String} erasmusCode - The erasmus code of the host organization to delete
+ * @param {Object} res - The HTTP res
+ * @returns {Boolean}  - This method returns true if the delete of host organization was successfull, else false
+ */
+exports.deleteHostOrg = function (erasmuscode, res) {
+  return new Promise(function (resolve, reject) {
+    var delHost = OrganizationModel.deleteHostOrg(erasmuscode)
+    delHost.then(function (result) {
+      if (!result) {
+        resolve(false)
+      } else {
+        resolve(true)
+      }
+    })
+  })
+}
+
+/**
+ * This method deletes an external tutor
+ * @param {String} email - The external tutor's email
+ * @param {Object} res - The HTTP res
+ * @returns {Boolean}  - This method returns true if the delete of external tutor was successfull, else false
+ */
+exports.deleteExTutor = function (email, res) {
+  return new Promise(function (resolve, reject) {
+    var delExTutor = ExternalTutorModel.deleteExTutor(email)
+    delExTutor.then(function (result) {
+      if (!result) {
+        resolve(false)
+      } else {
+        resolve(true)
+      }
+    })
+  })
+}
+
+/**
+ * This method gets an host organization by id
+ * @param {String} id - The id of Host organization
+ * @returns {JSON} - It return an JSON Object containing the informations of the host organization
+ */
+exports.getHostOrganization = function (id) {
+  return new Promise(function (resolve, reject) {
+    var get = OrganizationModel.retrieveOne(id)
+    get.then(function (result) {
+      resolve({ ErasmusCode: result.getErasmusCode(), Faculty: result.getFaculty(), Address: result.getAddress(), Size: result.getOrgSize(), Country: result.getCountry(), Contacts: result.getContacts(), Name: result.getName() })
+    })
+  })
+}
---
+++ test/documentTest.js
@@ -0,0 +1,133 @@
+var chai = require('chai')
+var expect = chai.expect
+var documentControl = require('../app/controllers/documentControl')
+
+describe('Field test for documentControl', function () {
+  it('Testing insertIDCard', function (done) {
+    var check = documentControl.idHandler('d.devito@studenti.unisa.it')
+    check.then(function (result) {
+      expect(result).to.not.be.null
+      done()
+    })
+  })
+
+  it('Testing insertIDCard already exist', function (done) {
+    var check = documentControl.idHandler('d.devito@studenti.unisa.it')
+    check.then(function (result) {
+      expect(result).to.not.be.null
+      done()
+    })
+  })
+
+  it('Testing insertCV', function (done) {
+    var check = documentControl.cvHandler('d.devito@studenti.unisa.it')
+    check.then(function (result) {
+      expect(result).to.not.be.null
+      done()
+    })
+  })
+
+  it('Testing insertCV already exist', function (done) {
+    var check = documentControl.cvHandler('d.devito@studenti.unisa.it')
+    check.then(function (result) {
+      expect(result).to.not.be.null
+      done()
+    })
+  })
+
+  it('Testing viewIDCard', function (done) {
+    var check = documentControl.idHandler('f.califano@studenti.unisa.it')
+    check.then(function (result) {
+      expect(result).to.not.be.null
+      var check = documentControl.viewID('f.califano@studenti.unisa.it')
+      check.then(function (result) {
+        expect(result).to.not.be.null
+        done()
+      })
+    })
+  })
+
+  it('Testing viewIDCard not exist', function (done) {
+    var check = documentControl.viewID('l.deamicis@studenti.unisa.it')
+    check.then(function (result) {
+      expect(result).to.not.be.null
+      done()
+    })
+  })
+
+  it('Testing viewCV', function (done) {
+    var check = documentControl.cvHandler('f.califano@studenti.unisa.it')
+    check.then(function (result) {
+      expect(result).to.not.be.null
+      var check = documentControl.viewCV('f.califano@studenti.unisa.it')
+      check.then(function (result) {
+        expect(result).to.not.be.null
+        done()
+      })
+    })
+  })
+
+  it('Testing viewCV not exist', function (done) {
+    var check = documentControl.viewCV('l.deamicis@studenti.unisa.it')
+    check.then(function (result) {
+      expect(result).to.not.be.null
+      done()
+    })
+  })
+
+  it('Testing getIDCardStatus', function (done) {
+    var check = documentControl.getIDState('d.devito@studenti.unisa.it')
+    check.then(function (result) {
+      expect(result).to.not.be.null
+      done()
+    })
+  })
+
+  it('Testing getIDCardStatus not exist', function (done) {
+    var check = documentControl.getIDState('l.deamicis@studenti.unisa.it')
+    check.then(function (result) {
+      expect(result).to.not.be.null
+      done()
+    })
+  })
+
+  it('Testing getCVStatus', function (done) {
+    var check = documentControl.getCVState('d.devito@studenti.unisa.it')
+    check.then(function (result) {
+      expect(result).to.not.be.null
+      done()
+    })
+  })
+
+  it('Testing getCVStatus not exist', function (done) {
+    var check = documentControl.getCVState('l.deamicis@studenti.unisa.it')
+    check.then(function (result) {
+      expect(result).to.not.be.null
+      done()
+    })
+  })
+
+  it('Testing deleteIDCard', function (done) {
+    var check = documentControl.idHandler('d.marino@studenti.unisa.it')
+    check.then(function (result) {
+      expect(result).to.not.be.null
+      var check = documentControl.IDEraser('d.marino@studenti.unisa.it')
+      check.then(function (result) {
+        expect(result).to.not.be.null
+        done()
+      })
+    })
+  })
+
+  it('Testing deleteCV', function (done) {
+    var check = documentControl.cvHandler('d.marino@studenti.unisa.it')
+    check.then(function (result) {
+      expect(result).to.not.be.null
+      var check = documentControl.CVEraser('d.marino@studenti.unisa.it')
+      check.then(function (result) {
+        expect(result).to.not.be.null
+        done()
+      })
+    })
+  })
+})
